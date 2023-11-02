a9957511df649f79691a8d63fa11e2e25f6a2e35
Sarah <sarah.zhang@sentify.co>
2023-11-02 15:21:14 +1300

    Add project files.

---
+++ LearningThought.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningThought.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
